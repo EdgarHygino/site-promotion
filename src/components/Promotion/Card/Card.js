bc5711b42b1dbe4c307cd75d7bff12341b7debc2
Edgar <edgar.hygino@gmail.com>
2020-09-27 18:31:41 -0300

    criado medoto Router-DOM e pages Root e Search

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -15,7 +15,7 @@ const PromotionCard = ({ promotion }) => (
                 {promotion.comments.length} {' '}
                 {promotion.comments.length > 1 ? "Comentários" : "Comentário"}
             </div>
-            <a href={promotion.url} target="_blank" className='promotion-card__link'>IR PARA O SITE</a>
+            <a href={promotion.url} target="_blank" rel="noopener noreferrer" className='promotion-card__link'>IR PARA O SITE</a>
             </footer>
         </div>
     </div>
