f6c7ccbf72ccb713e44247953a70507dcdd74aa0
EdgarHygino <edgar.hygino@gmail.com>
2020-09-29 21:42:31 -0300

    developer-1: include search and clean in files

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -1,21 +1,39 @@
 import React, {useState, useEffect} from 'react';
-import PromotionCard from '../Card/Card';
+import PromotionList from '../List/list';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
+import './Search.css';
 
 
 const PromotionSearch = () => {
     const [promotions, setPromotions] = useState([]);
+    const [search, setSearch] = useState('');
+
+
     useEffect(() => {
-        axios.get('http://localhost:5000/promotions?_embed=comments')
+        const params = {};
+        if (search) {
+            params.title_like = search;
+        }
+        axios.get('http://localhost:5000/promotions?_embed=comments', { params })
             .then((response) => {
                 setPromotions(response.data)
         });
-    }, []);
+    }, [search]);
     return (
-        <div>
-            {promotions.map((promotion) => (
-            <PromotionCard promotion={promotion} />
-            ))}
+        <div className='promotion-search'>
+            <header className='promotion-search__header'>
+                <h1>Promo Show</h1>
+                <Link to='/create'>Nova Promomoção</Link>    
+            </header>
+                <input 
+                    type='search' 
+                    className='promotion-search__input' 
+                    placeholder="Buscar" 
+                    value={search}
+                    onChange={(ev) => setSearch(ev.target.value)}
+                    />
+            <PromotionList promotions={promotions} loanding={!promotions.length} />
         </div>
     );
 };
