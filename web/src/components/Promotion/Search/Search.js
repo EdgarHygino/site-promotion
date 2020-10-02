7381c809f9ca94d477192b0682c8c24851669337
EdgarHygino <edgar.hygino@gmail.com>
2020-10-02 19:52:21 -0300

    developer-2: implantado a função formulario para incluir promoções

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -15,7 +15,7 @@ const PromotionSearch = () => {
         if (search) {
             params.title_like = search;
         }
-        axios.get('http://localhost:5000/promotions?_embed=comments', { params })
+        axios.get('http://localhost:5000/promotions?_embed=comments&_order=desc&_sort=id', { params })
             .then((response) => {
                 setPromotions(response.data)
         });
@@ -23,8 +23,8 @@ const PromotionSearch = () => {
     return (
         <div className='promotion-search'>
             <header className='promotion-search__header'>
-                <h1>Promo Show</h1>
-                <Link to='/create'>Nova Promomoção</Link>    
+                <h1>Canal de Descontos</h1>
+                <Link className='promotion-search__nova-promocao' to='/create'>Nova Promomoção</Link>    
             </header>
                 <input 
                     type='search' 
